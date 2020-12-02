ee21d957a5aa87bf17227c80a2e42b00833c2a96
Ivan Mitkov <ivan.mitkov@gmail.com>
2020-12-02 11:37:32 +0200

    Updates

--- src/events/order-created.ts
+++ src/events/order-created.ts
@@ -4,6 +4,7 @@ export interface OrderCreatedEvent {
   subject: Subjects.OrderCreated;
   data: {
     id: string;
+    version:number;
     status: OrderStatus;
     userId: string;
     //OrderCreatedEvent is converted to string so Date became string
